007b5a13c8cd33d28f586096ad8f48b562461200
mistakia <1823355+mistakia@users.noreply.github.com>
2022-05-28 14:33:32 -0400

    feat: cleanup traffic shaper

--- scripts/traffic-shaper.sh
+++ scripts/traffic-shaper.sh
@@ -32,7 +32,7 @@ function update_ipset {
 
     # Get IPs from DNS
     dig "$DNS_HOSTNAME" A +short | while read ip; do
-        # Add IPs to IP set
+        # Add ip to ipset
         $IPSET add half-prs $ip
     done
 }
@@ -41,23 +41,23 @@ function whitelist {
     install_ipset
     clear_iptables
 
-    # Allow connections from IP set
+    # Allow connections from ipset
     $IPTABLES -I INPUT -p tcp --dport $PORT -m set --match-set half-prs src -j ACCEPT
     $IPTABLES -I INPUT -p tcp --sport $PORT -m set --match-set half-prs src -j ACCEPT
 
-    # Allow connection to IP set
+    # Allow connection to ipset
     $IPTABLES -I OUTPUT -p tcp --dport $PORT -m set --match-set half-prs dst -j ACCEPT
     $IPTABLES -I OUTPUT -p tcp --sport $PORT -m set --match-set half-prs dst -j ACCEPT
 
     # Deny other incoming connections
-    $IPTABLES -A INPUT -p tcp --dport $PORT -m limit --limit 10/s -j ACCEPT
-    $IPTABLES -A INPUT -p tcp --sport $PORT -m limit --limit 10/s -j ACCEPT
+    # $IPTABLES -A INPUT -p tcp --dport $PORT -m limit --limit 10/s -j ACCEPT
+    # $IPTABLES -A INPUT -p tcp --sport $PORT -m limit --limit 10/s -j ACCEPT
     $IPTABLES -A INPUT -p tcp --dport $PORT -j DROP
     $IPTABLES -A INPUT -p tcp --sport $PORT -j DROP
 
     # Deny other outgoing conenctions
-    $IPTABLES -A OUTPUT -p tcp --dport $PORT -m limit --limit 10/s -j ACCEPT
-    $IPTABLES -A OUTPUT -p tcp --sport $PORT -m limit --limit 10/s -j ACCEPT
+    # $IPTABLES -A OUTPUT -p tcp --dport $PORT -m limit --limit 10/s -j ACCEPT
+    # $IPTABLES -A OUTPUT -p tcp --sport $PORT -m limit --limit 10/s -j ACCEPT
     $IPTABLES -A OUTPUT -p tcp --dport $PORT -j DROP
     $IPTABLES -A OUTPUT -p tcp --sport $PORT -j DROP
 }
@@ -74,7 +74,6 @@ function clear_tc {
     $TC qdisc del dev $IF root >/dev/null 2>&1
     $TC qdisc del dev $IF ingress >/dev/null 2>&1
     $TC qdisc del dev $IF_INGRESS root >/dev/null 2>&1
-    # $TC qdisc del dev $IF_INGRESS ingress
 }
 
 function clear_iptables {
@@ -98,20 +97,20 @@ function tc_ingress {
     # create ingress on external interface
     $TC qdisc add dev $IF handle ffff: ingress
 
-    # filter ingress packet to ingress interface
+    # redirect ingress packet to ingress interface
     $TC filter add dev $IF parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev $IF_INGRESS
 
-    # create root qdisc for ingress on the IFB device
+    # create root qdisc for ingress on the IFB device, default use general high priority class
     $TC qdisc add dev $IF_INGRESS root handle 1:0 htb default 10
 
     # create parent class for ingress
     $TC class add dev $IF_INGRESS parent 1: classid 1:1 htb rate $MAX
 
-    # create class for default high priority ingress traffic
+    # create class for general high priority ingress traffic
     $TC class add dev $IF_INGRESS parent 1:1 classid 1:10 htb rate $MAX
     # create class for high priority half PR ingress traffic
     $TC class add dev $IF_INGRESS parent 1:1 classid 1:30 htb rate $MAX
-    # create class for low priority ingress traffic
+    # create class for low priority nano network ingress traffic
     $TC class add dev $IF_INGRESS parent 1:1 classid 1:50 htb rate $RATE ceil $CEIL
 
     # filter high priority packets matching dns ips and send to classid 1:30
@@ -119,38 +118,33 @@ function tc_ingress {
         $TC filter add dev $IF_INGRESS parent 1:0 protocol ip prio 1 u32 match ip src $ip classid 1:30
     done
 
-    # filter low priority packets matching port send to classid 1:50
+    # filter low priority packets matching nano network port send to classid 1:50
     $TC filter add dev $IF_INGRESS parent 1:0 protocol ip prio 2 u32 match ip dport $PORT 0xffff classid 1:50
     $TC filter add dev $IF_INGRESS parent 1:0 protocol ip prio 2 u32 match ip sport $PORT 0xffff classid 1:50
 }
 
 function tc_egress {
-    # create root qdisc for egress, default use low priority class 20
+    # create root qdisc for egress, default use general high priority class 10
     $TC qdisc add dev $IF root handle 1:0 htb default 10
 
     # create parent class for egress
     $TC class add dev $IF parent 1: classid 1:1 htb rate $MAX
 
-    # create class for high priority half PR traffic
+    # create class for high priority general egress traffic
     $TC class add dev $IF parent 1:1 classid 1:10 htb rate $MAX
-    # create class for high priority half PR traffic
+    # create class for high priority half PR egress traffic
     $TC class add dev $IF parent 1:1 classid 1:20 htb rate $MAX
-    # create class for low priority egress traffic
+    # create class for low priority nano network egress traffic
     $TC class add dev $IF parent 1:1 classid 1:40 htb rate $RATE ceil $CEIL
 
     # filter packets marked with handle 2 and send to classid 1:20
     $TC filter add dev $IF parent 1:0 prio 1 handle 2 fw classid 1:20
 
-    # filter packets matching port and send to classid 1:20
+    # filter packets matching nano network port and send to classid 1:20
     $TC filter add dev $IF parent 1:0 protocol ip prio 2 u32 match ip dport $PORT 0xffff classid 1:40
     $TC filter add dev $IF parent 1:0 protocol ip prio 2 u32 match ip sport $PORT 0xffff classid 1:40
-    # $TC filter add dev $IF parent 1:0 prio 2 handle 4 fw classid 1:40
 
-    # mark low priority traffic to a half-pr with handle 4 for deprioritization
-    # $IPTABLES -t mangle -A OUTPUT -p tcp --dport $PORT -j MARK --set-mark 4
-    # $IPTABLES -t mangle -A OUTPUT -p tcp --sport $PORT -j MARK --set-mark 4
-
-    # mark high priority traffic to a half-pr with handle 2 for prioritization
+    # mark high priority traffic in the half-pr ipset with handle 2 for prioritization
     $IPTABLES -t mangle -A OUTPUT -p tcp --sport $PORT -m set --match-set half-prs src -j MARK --set-mark 2
     $IPTABLES -t mangle -A OUTPUT -p tcp --sport $PORT -m set --match-set half-prs dst -j MARK --set-mark 2
     $IPTABLES -t mangle -A OUTPUT -p tcp --dport $PORT -m set --match-set half-prs src -j MARK --set-mark 2
